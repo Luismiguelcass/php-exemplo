c0ef681bf1d9e7cee5e1dd8a7ffee49590a6ce82
Luismiguelcass <273195@unifio.edu.br>
2025-08-21 01:35:21 +0000

    Aula 20/08/2025

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
-require '../vendor/autoload.php';
-
-use Application\Hello;
+$nome = $_GET['nome'];
+$santos = "Hello World! I'm learning PHP! $nome";
+$n1 = true;
+$n2 = false;
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -12,6 +13,8 @@ use Application\Hello;
 </head>
 <body>
     <h1>PHP & Mysql</h1>
-    <p><?= Hello::message() ?></p>
+    <p><?= $santos ?></p>
+    <h1>Teste</h1>
+    <?= $n1 . $n2 ?>
 </body>
 </html>
