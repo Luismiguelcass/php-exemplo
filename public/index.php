bb5ab49d6d54e2524c4cbf89fff52d4bd2c87d90
Luismiguelcass <273195@unifio.edu.br>
2025-08-21 23:39:21 +0000

    Aula 21/08/2025

--- public/index.php
+++ public/index.php
@@ -1,8 +1,9 @@
 <?php
-$nome = $_GET['nome'];
-$santos = "Hello World! I'm learning PHP! $nome";
-$n1 = true;
-$n2 = false;
+$word = $_GET['word'];
+$santos = "Hello World! I'm learning PHP! $word";
+
+$n1 = 20; #variáveis PHP são dinâmicas, você não precisa definir o tipo primitivo dela
+$n2 = 25;
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -15,6 +16,14 @@ $n2 = false;
     <h1>PHP & Mysql</h1>
     <p><?= $santos ?></p>
     <h1>Teste</h1>
-    <?= $n1 . $n2 ?>
+    <?= $n1 . $n2 ?> <!--concatenação (não serve somente para strings)-->
+    <p>----------</p>
+    <?= $n1 + $n2 ?> <!--soma-->
+    <p>----------</p>
+    <?= $n1 - $n2 ?> <!--subtração-->
+    <p>----------</p>
+    <?= $n1 * $n2 ?> <!--multiplicação-->
+    <p>----------</p>
+    <?= $n1 / $n2 ?> <!--divisão-->
 </body>
 </html>
